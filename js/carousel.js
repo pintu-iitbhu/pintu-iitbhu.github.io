f1ccd61d287361197c5c440abc8ee6cadf4be769
pkyadav73199 <pintukumaryadav.min18@itbhu.ac.in>
2020-08-29 11:16:56 +0530

    myFirst commit

---
+++ js/carousel.js
@@ -0,0 +1,63 @@
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     nav: true,
+//     navText: [
+//       "<i class='fa fa-caret-left'></i>",
+//       "<i class='fa fa-caret-right'></i>"
+//     ],
+//     autoplay: true,
+//     autoplayHoverPause: true,
+//     responsive: {
+//       0: {
+//         items: 1
+//       },
+//       600: {
+//         items: 3
+//       },
+//       1000: {
+//         items: 3
+//       }
+//     }
+//   })
+
+// $('.owl-carousel').owlCarousel({
+//   loop:true,
+//   margin:10,
+//   responsiveClass:true,
+//   responsive:{
+//       0:{
+//           items:1,
+//           nav:true
+//       },
+//       600:{
+//           items:3,
+//           nav:false
+//       },
+//       1000:{
+//           items:3,
+//           nav:true,
+//           loop:false
+//       }
+//   }
+// })
+	// $("#owl-slider").owlCarousel({
+  //       navigation: false,
+  //       pagination: true,
+  //       itemsCustom : [
+	//         [0, 1],
+	//         [700, 2],
+	//         [960, 3]
+	//      ],
+  //       navigationText: false
+  //   });
+//   $("#owl-slider2").owlCarousel({
+//     navigation: false,
+//     pagination: true,
+//     itemsCustom : [
+//       [0, 1],
+//       [700, 2],
+//       [960, 3]
+//    ],
+//     navigationText: false
+// });
